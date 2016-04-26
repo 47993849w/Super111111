9c7685df232ce5d95fdab9fe9e33708ccf16a725
47993849w <47993849w>
2016-04-26 03:51:40 +0200

    Añadidas varias funcones!

--- main.js
+++ main.js
@@ -968,7 +968,11 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: p
 function preload() {
     game.load.image('bullet', 'assets/sprites/purple_ball.png');
     game.load.image('phaser', 'assets/sprites/phaser-dude.png');
-    game.load.spritesheet('veggies', 'assets/sprites/fruitnveg32wh37.png', 32, 32);
+    game.load.spritesheet('veggies', 'assets/glossy-balls-hi.png', 78, 84);
+    game.load.audio('blaster', 'assets/audio/SoundEffects/blaster.mp3');
+    game.load.audio('yes', 'assets/audio/yeah.wav');
+    game.load.audio('no', 'assets/audio/nop.wav');
+    game.load.image('pic', 'assets/papel-pintado-liso-con-puntos-gris-y-fondo-blanco.jpg');
 }
 //var sprite;
 var bullets;
@@ -977,48 +981,115 @@ var group;
 var cursors;
 var fireRate = 100;
 var nextFire = 0;
-var i2;
+;
+var counter = 30;
+var text2;
+var text;
 var c;
+var guay;
+var blaster;
+var yes;
+var nop;
+var text3;
+var sc = 0;
 function create() {
+    counter = 30;
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.stage.backgroundColor = '#313130';
+    var s = game.add.tileSprite(0, 0, 800, 600, 'pic');
+    game.stage.backgroundColor = '#F2F2F2';
     bullets = game.add.group();
     bullets.enableBody = true;
     bullets.physicsBodyType = Phaser.Physics.ARCADE;
     bullets.createMultiple(50, 'bullet');
     bullets.setAll('checkWorldBounds', true);
     bullets.setAll('outOfBoundsKill', true);
-    var text = game.add.text(10, 10, "AMARILLO", { font: "65px Arial", fill: "#ff0044", align: "center" });
-    i2 = 1;
+    blaster = game.add.audio('blaster');
+    yes = game.add.audio('yes');
+    nop = game.add.audio('no');
+    guay = game.rnd.between(0, 5);
+    if (guay == 0) {
+        text = game.add.text(10, 10, "AZUL", { font: "65px Arial", fill: "#ff0044", align: "center" });
+    }
+    if (guay == 1) {
+        text = game.add.text(10, 10, "VERDE", { font: "65px Arial", fill: "#ff4344", align: "center" });
+    }
+    if (guay == 2) {
+        text = game.add.text(10, 10, "NARANJA", { font: "65px Arial", fill: "#ff6744", align: "center" });
+    }
+    if (guay == 3) {
+        text = game.add.text(10, 10, "LILA", { font: "65px Arial", fill: "#ff8744", align: "center" });
+    }
+    if (guay == 4) {
+        text = game.add.text(10, 10, "ROJO", { font: "65px Arial", fill: "#ff1111", align: "center" });
+    }
+    if (guay == 5) {
+        text = game.add.text(10, 10, "AMARILLO", { font: "65px Arial", fill: "#ff6744", align: "center" });
+    }
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.stage.backgroundColor = '#2d2d2d';
-    sprite1 = game.add.sprite(32, 200, 'phaser');
+    text3 = game.add.text(600, 10, "Score: " + sc, { font: "40px Arial", fill: "#530545", align: "center" });
+    sprite1 = game.add.sprite(400, 200, 'phaser');
     game.physics.arcade.enable(sprite1);
     group = game.add.physicsGroup(Phaser.Physics.ARCADE);
-    for (var i = 0; i < 10; i++) {
-        var c = group.create(game.rnd.between(100, 770), game.rnd.between(0, 570), 'veggies', i);
-        c.body.velocity.x = game.rnd.between(-40, 32);
+    this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    game.input.keyboard.addKeyCapture([Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT, Phaser.Keyboard.SPACEBAR]);
+    createball();
+    game.time.events.duration;
+    text2 = game.add.text(game.world.centerX, game.world.centerY, 'Counter: 0', { font: "64px Arial", fill: "#ffffff", align: "center" });
+    text2.anchor.setTo(0.5, 0.5);
+    cursors = game.input.keyboard.createCursorKeys();
+    game.time.events.loop(Phaser.Timer.SECOND, updateCounter, this);
+}
+function updateCounter() {
+    counter--;
+    text2.setText('Time: ' + counter);
+}
+function createball() {
+    for (var i = 0; i < 6; i++) {
+        var c = group.create(game.rnd.between(100, 770), game.rnd.between(0, 6), 'veggies', i);
+        c.body.velocity.x = game.rnd.between(40, 132);
         c.body.velocity.y = game.rnd;
         c.checkWorldBounds = true;
         c.body.collideWorldBounds = true;
         game.physics.enable(c, Phaser.Physics.ARCADE);
         c.body.bounce.set(1);
     }
-    game.time.events.duration;
-    cursors = game.input.keyboard.createCursorKeys();
-}
-function createball(group1) {
-    //group1.remove();
-    for (var i = 0; i < 10; i++) {
-        var c = group.create(game.rnd.between(100, 770), game.rnd.between(0, 570), 'veggies', game.rnd.between(0, 6));
-        c.body.velocity.x = 45;
-        ;
-    }
 }
 function update() {
+    if (this.spaceKey.isDown) {
+        group.callAll('kill');
+        counter = 30;
+        createball();
+    }
+    if (guay == 0) {
+        // text = game.add.text(10, 10, "AZUL", {font: "65px Arial", fill: "#ff0044", align: "center"});
+        text.text = 'azul';
+    }
+    if (guay == 1) {
+        text.text = "VERDE";
+    }
+    if (guay == 2) {
+        text.text = "NARANJA";
+    }
+    if (guay == 3) {
+        text.text = "LILA";
+    }
+    if (guay == 4) {
+        text.text = "ROJO";
+    }
+    if (guay == 5) {
+        text.text = "AMARILLO";
+    }
     if (game.physics.arcade.collide(bullets, group, collisionHandler, processHandler, this)) {
         console.log('boom');
     }
+    //bullets.checkWorldBounds=true;
+    //bullets.body.collideWorldBounds = true;
+    if (counter < 0) {
+        gameOver();
+    }
+    /*if (counter>60){
+        counter=60;
+    }*/
     game.physics.arcade.collide(group, group, colision, colision, this);
     // game.physics.arcade.overlap(sprite, group, collisionHandler, null, this);
     sprite1.body.velocity.x = 0;
@@ -1041,20 +1112,37 @@ function update() {
         fire();
     }
 }
+function gameOver() {
+    text2.setText('GAME OVER');
+    //game.input.onTap.addOnce(restart,this);
+    group.callAll('kill');
+    //create();
+}
 function colision(group, group1) {
-    group.body.bounce.set(0.8);
+    group.body.bounce.set(1);
+    group1.body.bounce.set(1);
 }
 function processHandler(player, veg) {
     return true;
 }
 function collisionHandler(bullet, veg) {
-    if (veg.frame == i2) {
+    if (veg.frame == guay) {
         veg.kill();
-        i2 = veg.Frame;
+        guay = game.rnd.between(0, 5);
+        yes.play();
+        counter = counter + 5;
+        text2.setText('Time: ' + counter);
+        sc++;
+        text3.setText('Score: ' + sc);
+    }
+    else {
+        counter = counter - 5;
+        text2.setText('Time: ' + counter);
+        nop.play();
     }
     veg.kill();
     c = group.create(game.rnd.between(100, 770), game.rnd.between(0, 570), 'veggies', veg.frame);
-    c.body.velocity.x = game.rnd.between(-40, 32);
+    c.body.velocity.x = game.rnd.between(40, 132);
     c.body.velocity.y = game.rnd;
     c.checkWorldBounds = true;
     c.body.collideWorldBounds = true;
@@ -1064,6 +1152,7 @@ function collisionHandler(bullet, veg) {
     bullet.kill();
 }
 function fire() {
+    blaster.play();
     if (game.time.now > nextFire && bullets.countDead() > 0) {
         nextFire = game.time.now + fireRate;
         var bullet = bullets.getFirstDead();
--- main.ts
+++ main.ts
@@ -19,7 +19,12 @@ function preload() {
 
     game.load.image('bullet', 'assets/sprites/purple_ball.png');
     game.load.image('phaser', 'assets/sprites/phaser-dude.png');
-    game.load.spritesheet('veggies', 'assets/sprites/fruitnveg32wh37.png', 32, 32);
+    game.load.spritesheet('veggies', 'assets/glossy-balls-hi.png', 78, 84);
+    game.load.audio('blaster', 'assets/audio/SoundEffects/blaster.mp3');
+    game.load.audio('yes','assets/audio/yeah.wav')
+    game.load.audio('no','assets/audio/nop.wav')
+
+    game.load.image('pic', 'assets/papel-pintado-liso-con-puntos-gris-y-fondo-blanco.jpg')
 }
 
 //var sprite;
@@ -29,13 +34,23 @@ var group;
 var cursors;
 var fireRate = 100;
 var nextFire = 0;
-var i2;
+;
+var counter = 30;
+var text2;
+var text;
 var c;
+var guay;
+var blaster;
+var yes;
+var nop;
+var text3;
+var sc = 0;
 function create() {
-
+    counter=30;
     game.physics.startSystem(Phaser.Physics.ARCADE);
+    var s = game.add.tileSprite(0, 0, 800, 600, 'pic');
 
-    game.stage.backgroundColor = '#313130';
+    game.stage.backgroundColor = '#F2F2F2';
 
     bullets = game.add.group();
     bullets.enableBody = true;
@@ -45,31 +60,79 @@ function create() {
     bullets.setAll('checkWorldBounds', true);
     bullets.setAll('outOfBoundsKill', true);
 
+    blaster = game.add.audio('blaster');
+    yes = game.add.audio('yes');
+nop=game.add.audio('no')
+    
+
+    guay = game.rnd.between(0,5);
+
+    if (guay==0) {
+         text = game.add.text(10, 10, "AZUL", {font: "65px Arial", fill: "#ff0044", align: "center"});
+    }
+
+    if (guay==1) {
+         text = game.add.text(10, 10, "VERDE", {font: "65px Arial", fill: "#ff4344", align: "center"});
+    }
+
+    if (guay==2) {
+         text = game.add.text(10, 10, "NARANJA", {font: "65px Arial", fill: "#ff6744", align: "center"});
+    }
+
+    if (guay==3) {
+         text = game.add.text(10, 10, "LILA", {font: "65px Arial", fill: "#ff8744", align: "center"});
+    }
+
+    if (guay==4) {
+         text = game.add.text(10, 10, "ROJO", {font: "65px Arial", fill: "#ff1111", align: "center"});
+    }
 
 
-
-
-
-    var text = game.add.text(10,10, "AMARILLO", { font: "65px Arial", fill: "#ff0044", align: "center" });
-    i2=1;
+    if (guay==5) {
+         text = game.add.text(10, 10, "AMARILLO", {font: "65px Arial", fill: "#ff6744", align: "center"});
+    }
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    game.stage.backgroundColor = '#2d2d2d';
 
-    sprite1 = game.add.sprite(32, 200, 'phaser');
+    text3 = game.add.text(600, 10, "Score: "+sc, {font: "40px Arial", fill: "#530545", align: "center"});
+
+
+    sprite1 = game.add.sprite(400, 200, 'phaser');
 
      game.physics.arcade.enable(sprite1);
 
     group = game.add.physicsGroup(Phaser.Physics.ARCADE);
 
 
+    this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    game.input.keyboard.addKeyCapture([ Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT, Phaser.Keyboard.SPACEBAR ]);
 
 
-    for (var i = 0; i < 10; i++)
+   createball();
+
+   game.time.events.duration;
+
+    text2 = game.add.text(game.world.centerX, game.world.centerY, 'Counter: 0', { font: "64px Arial", fill: "#ffffff", align: "center" });
+    text2.anchor.setTo(0.5, 0.5);
+    cursors = game.input.keyboard.createCursorKeys();
+
+    game.time.events.loop(Phaser.Timer.SECOND, updateCounter, this);
+
+}
+
+function updateCounter() {
+
+    counter--;
+
+    text2.setText('Time: ' + counter);
+
+}
+function createball(){
+    for (var i = 0; i < 6; i++)
     {
-        var c = group.create(game.rnd.between(100, 770), game.rnd.between(0, 570), 'veggies', i);
+        var c = group.create(game.rnd.between(100, 770), game.rnd.between(0, 6), 'veggies', i);
 
-        c.body.velocity.x=game.rnd.between(-40,32);
+        c.body.velocity.x=game.rnd.between(40,132);
         c.body.velocity.y = game.rnd;
         c.checkWorldBounds=true;
         c.body.collideWorldBounds = true;
@@ -80,33 +143,53 @@ function create() {
 
 
     }
-
-   game.time.events.duration;
-
-
-    cursors = game.input.keyboard.createCursorKeys();
-
-}
-
-
-function createball(group1){
-    //group1.remove();
-    for (var i = 0; i < 10; i++)
-    {
-        var c = group.create(game.rnd.between(100, 770), game.rnd.between(0, 570), 'veggies', game.rnd.between(0, 6));
-        c.body.velocity.x=45;;
-    }
 }
 
 
 function update() {
 
+    if (this.spaceKey.isDown){
+        group.callAll('kill');
+        counter=30;
+        createball();
+    }
 
+    if (guay==0) {
+        // text = game.add.text(10, 10, "AZUL", {font: "65px Arial", fill: "#ff0044", align: "center"});
+        text.text='azul';
+    }
+
+    if (guay==1) {
+         text.text="VERDE";
+    }
+    if (guay==2) {
+         text.text= "NARANJA";
+    }
+    if (guay==3) {
+         text.text=  "LILA";
+    }
+    if (guay==4) {
+         text.text =  "ROJO";
+    }
+    if (guay==5) {
+         text.text =  "AMARILLO";
+    }
     if (game.physics.arcade.collide(bullets, group, collisionHandler, processHandler, this))
     {
         console.log('boom');
     }
 
+    //bullets.checkWorldBounds=true;
+    //bullets.body.collideWorldBounds = true;
+
+    if (counter<0){
+        gameOver();
+    }
+
+
+    /*if (counter>60){
+        counter=60;
+    }*/
 
     game.physics.arcade.collide(group, group,colision,colision, this);
 
@@ -146,8 +229,17 @@ function update() {
 
 
 
+function  gameOver(){
+    text2.setText('GAME OVER')
+    //game.input.onTap.addOnce(restart,this);
+
+    group.callAll('kill');
+    //create();
+
+}
 function colision(group, group1){
-    group.body.bounce.set(0.8);
+    group.body.bounce.set(1);
+    group1.body.bounce.set(1);
 }
 
 function processHandler (player, veg) {
@@ -158,19 +250,28 @@ function processHandler (player, veg) {
 
 function collisionHandler (bullet, veg) {
 
-    if (veg.frame == i2)
+    if (veg.frame == guay)
     {
         veg.kill();
 
-        i2=veg.Frame;
+        guay=game.rnd.between(0,5);
+        yes.play()
+        counter = counter+5;
 
+        text2.setText('Time: '+counter);
+        sc++;
+text3.setText('Score: '+sc);
+    }
 
-
+    else{
+        counter=counter-5;
+        text2.setText('Time: ' + counter);
+nop.play();
     }
 
     veg.kill();
     c = group.create(game.rnd.between(100, 770), game.rnd.between(0, 570), 'veggies', veg.frame);
-    c.body.velocity.x=game.rnd.between(-40,32);
+    c.body.velocity.x=game.rnd.between(40,132);
     c.body.velocity.y = game.rnd;
     c.checkWorldBounds=true;
     c.body.collideWorldBounds = true;
@@ -184,6 +285,8 @@ function collisionHandler (bullet, veg) {
 }
 
 function fire() {
+
+    blaster.play();
 
     if (game.time.now > nextFire && bullets.countDead() > 0)
     {
